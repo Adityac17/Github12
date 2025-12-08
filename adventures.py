a4360cf992e72e0740c07666d066fd9e68a95429
Aditya Sunil Chouksey <2025.adityac@isu.ac.in>
2025-12-08 22:49:12 +0530

    Fix typo in input prompt for chosen exit

--- adventures.py
+++ adventures.py
@@ -1,8 +1,8 @@
 available_exits = ["north", "south", "east", "west"]
-
+# this is a escape game using while loop iteration
 chosen_exit = ""
 while chosen_exit not in available_exits:
-    chosen_exit = input("Pleas eenter a direction to exit: ")
+    chosen_exit = input("Please enter a direction to exit: ")
     if chosen_exit.casefold() == "quit":
         print("Game Over")
         break
