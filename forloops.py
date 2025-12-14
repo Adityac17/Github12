7cab35b339dd9766cdb9f52bf54880d5361fd0f9
Aditya Sunil Chouksey <2025.adityac@isu.ac.in>
2025-12-14 21:47:49 +0530

    another way to write this program
    added another way to write this program

--- forloops.py
+++ forloops.py
@@ -12,3 +12,26 @@ for char in number:
         sep = sep + char
 # another way to write line 12 is separator_list.append(char) which does exactly the same thing
 print(sep)
+
+# After learning more about Python i have figured out another way to write this program
+"""
+def print_parrot_chars(text):
+    for character in text:
+        print(character)
+
+def extract_separators(input_string):
+    separator_list = []
+    for char in input_string:
+        if not char.isnumeric():
+            separator_list.append(char)
+    return "".join(separator_list)
+
+# Main Execution Guard
+if __name__ == "__main__":
+    parrot = "Norwegian Blue"
+    print_parrot_chars(parrot)
+
+    raw_input_string = input("Please enter a series of number with any no of separators you like: ")
+    result = extract_separators(raw_input_string)
+    print(result)
+    """
