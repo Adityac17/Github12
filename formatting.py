8adf312b1eeaf8d952180677e5da60a8625c8825
Aditya Sunil Chouksey <2025.adityac@isu.ac.in>
2025-12-15 12:07:29 +0530

    Refactor formatting.py to use constants
    Replaced hardcoded values with constants for max range and Pi approximation.

--- formatting.py
+++ formatting.py
@@ -1,32 +1,37 @@
-for i in range(1, 13):
+max_range = 13
+max_pie_v = 22/7
+# Defining constants at the top
+
+
+for i in range(1, max_range):
     print("No. {0:2} squared is {1:4} and cubed is {2:4}".format(i, i ** 2, i ** 3 ))
 
 print()
 
-for i in range(1, 13):
+for i in range(1, max_range):
     print("No. {0:2} squared is {1:3} and cubed is {2:4}".format(i, i ** 2, i ** 3 ))
 
 print()
 
-for i in range(1, 13):
+for i in range(1, max_range):
     print("No. {0:2} squared is {1:<3} and cubed is {2:<4}".format(i, i ** 2, i ** 3 ))
 
 print()
 
-for i in range(1, 13):
+for i in range(1, max_range):
     print("No. {0:2} squared is {1:^3} and cubed is {2:^4}".format(i, i ** 2, i ** 3 ))
 
 print()
 
-for i in range(1, 13):
+for i in range(1, max_range):
     print("No. {0:2} squared is {1:>3} and cubed is {2:>4}".format(i, i ** 2, i ** 3 ))
 
 print()
 
-print("Pi is approximately {0:12}".format( 22/7))
-print("Pi is approximately {0:12f}".format( 22/7))
-print("Pi is approximately {0:12.50f}".format( 22/7))
-print("Pi is approximately {0:52.50f}".format( 22/7))
-print("Pi is approximately {0:62.50f}".format( 22/7))
-print("Pi is approximately {0:72.50f}".format( 22/7))
+print("Pi is approximately {0:12}".format( max_pie_v ))
+print("Pi is approximately {0:12f}".format( max_pie_v))
+print("Pi is approximately {0:12.50f}".format( max_pie_v))
+print("Pi is approximately {0:52.50f}".format( max_pie_v))
+print("Pi is approximately {0:62.50f}".format( max_pie_v))
+print("Pi is approximately {0:72.50f}".format( max_pie_v))
 
