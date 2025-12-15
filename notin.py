22c781e171f2314ed22dd9e6144522346027ad29
Aditya Sunil Chouksey <2025.adityac@isu.ac.in>
2025-12-15 19:08:35 +0530

    Refactor activity check to use TARGET_ACTIVITY constant
    Refactored code to define TARGET_ACTIVITY constant for improved readability and maintainability.

--- notin.py
+++ notin.py
@@ -4,3 +4,19 @@ if "playsole" not in activity.casefold():
     print("But i wanna go to playsole")
 else:
     print("Yay! Let's go to playsole!")
+
+'''
+This is the 2nd way to type the code a bit more advanced 
+
+# Define the target activity once
+TARGET_ACTIVITY = "playsole"
+
+activity = input("What would you like to do today? ")
+
+if TARGET_ACTIVITY not in activity.casefold():
+    print(f"But i wanna go to {TARGET_ACTIVITY}")
+else:
+    print(f"Yay! Let's go to {TARGET_ACTIVITY}!")
+
+running this code will still give the same output as the one in lines 1 to 6
+'''
