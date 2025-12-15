f38f515acf052b346e094f8c47a2942c58b5b91e
Aditya Sunil Chouksey <2025.adityac@isu.ac.in>
2025-12-15 19:07:30 +0530

    Fix indentation and add else statement for activity

--- notin.py
+++ notin.py
@@ -2,3 +2,5 @@ activity = input("What would you like you to do today? ")
 
 if "playsole" not in activity.casefold():
     print("But i wanna go to playsole")
+else:
+    print("Yay! Let's go to playsole!")
